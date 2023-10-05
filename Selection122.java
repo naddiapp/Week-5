fe89c7698c53041fdead6267e5463c33fb11b359
naddiapp <nadiapramudita43@gmail.com>
2023-10-05 22:48:16 +0700

    Selection222.java

--- Selection122.java
+++ Selection122.java
@@ -5,10 +5,15 @@ public class Selection122 {
         Scanner input22 = new Scanner(System.in);
         System.out.print("Input a number = ");
         int number = input22.nextInt();
-        if(number%2 == 0){
-            System.out.println(number+" is an even number!");
-        }else{
-            System.out.println(number+" is an odd number!");
-        }
+        String result;
+
+// if (number % 2 == 0) {
+ // System.out.println("number "+number+ even number");
+ //}
+ //else{
+
+result = (number %2 == 0) ? "even number" : "odd number";
+System.out.println(result);
     }
-}
+}        
+        
